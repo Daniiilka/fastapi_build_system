0e9467de74e6cd220a71b2d6174750da1459374a
daniil <denrds@gmail.com>
2023-06-30 17:56:45 +0400

    fixed path in script for tmux session

--- tmux-build-system.sh
+++ tmux-build-system.sh
@@ -5,7 +5,7 @@ tmux -S new-session -d -s $session
 window=0
 tmux -S rename-window -t $session:$window '5000-build-system'
 tmux -S send-keys -t $session:$window 'cd {PATH TO THE PROJECT}' C-m
-tmux -S /home/dev/socket send-keys -t $session:$window 'source venv/bin/activate' C-m
-tmux -S /home/dev/socket send-keys -t $session:$window 'uvicorn main:app --reload --port 5000' C-m
+tmux -S send-keys -t $session:$window 'source venv/bin/activate' C-m
+tmux -S send-keys -t $session:$window 'uvicorn main:app --reload --port 5000' C-m
 
 
